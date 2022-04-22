5ce4dd5178f4afff4c038a0e6c934f811b236733
vijay shaaruck <vijay@onedpo.com>
2022-04-22 19:22:28 +0530

    initial project structure

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  exports: [FlexLayoutModule],
+  declarations: [],
+  imports: [CommonModule],
+})
+export class MaterialModule {}
